79d4eaaee4fcb5bddcc27aa3e82622e6e1f6e7ed
ShubhamThakre <shhubhamthackeray@gmail.com>
2022-01-11 23:02:07 +0530

    bug fixes | Header section in progress

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ function App() {
   }, [])
   return (
     <div className="App">
-        hello
+        hellosdsdsdds
         {products && products.map((product: ProductsFields) => <p key={JSON.stringify(product.id)}>{product.title}</p>)}
     </div>
   );
